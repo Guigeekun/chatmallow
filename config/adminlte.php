4633b042c499c91de0cd07b0308d6a6945e5c181
Kyuminn <azerty153486@outlook.fr>
2017-03-01 17:55:59 +0100

    Menu update + fix logo
    Dernier fix pour le logo + réorganisation du side-menu

--- config/adminlte.php
+++ config/adminlte.php
@@ -111,72 +111,16 @@ return [
     'menu' => [
         'SUIVI',
         [
-            'text' => 'Blog',
-            'url'  => 'admin/blog',
-        ],
-        [
             'text'        => 'Graphique',
-            'url'         => 'admin/pages',
+            'url'         => '/stats',
             'icon'        => 'area-chart',
             'label'       => 4,
             'label_color' => 'success',
         ],
-        'SETTINGS',
         [
             'text' => 'Cats',
             'url'  => '/cats',
             'icon' => 'paw',
-        ],
-        [
-            'text'    => 'Multilevel',
-            'icon'    => 'share',
-            'submenu' => [
-                [
-                    'text' => 'Level One',
-                    'url'  => '#',
-                ],
-                [
-                    'text'    => 'Level One',
-                    'url'     => '#',
-                    'submenu' => [
-                        [
-                            'text' => 'Level Two',
-                            'url'  => '#',
-                        ],
-                        [
-                            'text'    => 'Level Two',
-                            'url'     => '#',
-                            'submenu' => [
-                                [
-                                    'text' => 'Level Three',
-                                    'url'  => '#',
-                                ],
-                                [
-                                    'text' => 'Level Three',
-                                    'url'  => '#',
-                                ],
-                            ],
-                        ],
-                    ],
-                ],
-                [
-                    'text' => 'Level One',
-                    'url'  => '#',
-                ],
-            ],
-        ],
-        'LABELS',
-        [
-            'text'       => 'Important',
-            'icon_color' => 'red',
-        ],
-        [
-            'text'       => 'Warning',
-            'icon_color' => 'yellow',
-        ],
-        [
-            'text'       => 'Information',
-            'icon_color' => 'aqua',
         ],
     ],
 
